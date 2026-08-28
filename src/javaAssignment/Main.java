d6d6b4164c2833991a5c13d1a927263978024838
Eng.Hosny Almasrii <hosnyhassan2004@gmail.com>
2026-08-28 15:24:29 +0300

    Implement Main class to test NumberAnalyzer methods
    This class serves as the entry point for the program, testing various methods of the NumberAnalyzer class sequentially.

--- src/javaAssignment/Main.java
+++ src/javaAssignment/Main.java
@@ -1,4 +1,37 @@
 package javaAssignment;
 
-public class main {
+/**
+ * This class is the entry point of the program.
+ * It tests all the methods of the NumberAnalyzer class in sequence.
+ */
+public class Main {
+
+    /**
+     * The main method used to run and test all NumberAnalyzer methods step by step.
+     *
+     * @param args the command line arguments (not used in this program).
+     */
+    public static void main(String[] args) {
+
+        // Step 1: Testing Method 1
+        System.out.println("--- Testing Method 1---");
+        NumberAnalyzer.printNumbersDescending();
+
+        // Step 2: Testing Method 2
+        System.out.println("--- Testing Method 2---");
+        NumberAnalyzer.printEvenOrOdd();
+
+        // Step 3: Testing Method 3
+        System.out.println("--- Testing Method 3---");
+        int testNumber = 5;
+        String typeResult = NumberAnalyzer.checkNumberType(testNumber);
+        System.out.println("The number " + testNumber + " is: " + typeResult);
+
+        // Step 4: Testing Method 4
+        System.out.println("--- Testing Method 4---");
+        int num1 = 10;
+        int num2 = 20;
+        int sumResult = NumberAnalyzer.calculateSum(num1, num2);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sumResult);
+    }
 }
